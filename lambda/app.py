d62ef18b8aa3b7e6f4c443193ac3f2202793eb29
Vinay Selvaraj <vinay@selvaraj.com>
2021-01-08 14:44:56 -0500

    Fixed template and setting LD_LIBRARY_PATH

--- lambda/app.py
+++ lambda/app.py
@@ -2,6 +2,7 @@ import os
 import json
 from postal.expand import expand_address
 
+os.environ['LD_LIBRARY_PATH'] = '/usr/local/lib'
 
 def handler(event, context):
     result = expand_address('Quatre vingt douze Ave des Champs-Élysées')
