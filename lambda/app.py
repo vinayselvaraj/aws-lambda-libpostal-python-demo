a90535c1b0f968a2dd9a52b2b50df868f0487330
Vinay Selvaraj <vinay@selvaraj.com>
2021-01-08 16:40:57 -0500

    Updated parser

--- lambda/app.py
+++ lambda/app.py
@@ -1,9 +1,10 @@
 import os
 import json
-from postal.expand import expand_address
 from postal.parser import parse_address
 
 def handler(event, context):
-    result = parse_address(event['address'])
-    print(result)
+    parse_output = parse_address(event['address'])
+    result = dict()
+    for i in parse_output:
+      output[i[1]] = i[0]
     return json.dumps(result)
