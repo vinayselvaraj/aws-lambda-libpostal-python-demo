c98c6c5c7cc44b77b3b370ef532efb3e8f460ba1
Vinay Selvaraj <vinay@selvaraj.com>
2021-01-08 17:02:51 -0500

    Fixed bug

--- lambda/app.py
+++ lambda/app.py
@@ -6,5 +6,5 @@ def handler(event, context):
     parse_output = parse_address(event['address'])
     result = dict()
     for i in parse_output:
-      output[i[1]] = i[0]
+      result[i[1]] = i[0]
     return json.dumps(result)
